663124ab170139c954bf5ee535d7a9aab860fa9e
Leonard Contreras <leonard.contreras03@gmail.com>
2022-08-02 15:43:14 -0500

    passwords exercise

---
+++ seeder.sql
@@ -0,0 +1,10 @@
+USE adlister_db;
+
+
+
+
+INSERT INTO ads (user_id, title, description, price_in_cents)
+VALUES ('1', 'car', 'red corvette', '500000'),
+       ('1', 'boat', 'small boat', '1000'),
+       ('1', 'shirt', 'red shirt', '1000')
+
